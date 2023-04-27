4ec371e4a91d1d488902676bae894da9eceef719
Yana <sieghostienko@gmail.com>
2023-04-27 19:32:55 +0300

    Две задачи

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//Например: 
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+//Random rand = new Random();
+//int number = rand.Next(10, 100);
+//Console.WriteLine(number);
+// if(number / 10 > number % 10)
+// {
+//    Console.WriteLine($"{number} -> {number / 10}");
+ //}
+ //else
+ //{
+ //   Console.WriteLine($"{number} -> {number % 10}");
+// }
+
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+Console.WriteLine(rand.NextDouble());
---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит 3х значное число и удаляет вторую цифру этого числа
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine($"Трехзначное число {number}");
+int ic = number/100;
+int i = number % 10;
+int result = ic * 10 + i;
+System.Console.WriteLine($"{result}");
